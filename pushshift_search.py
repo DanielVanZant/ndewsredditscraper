fe74f31f6491ae1e1e39c9235235df1e28bd7769
Paul Morris <pmorris2012@fau.edu>
2020-09-23 21:24:00 -0400

    download script tested

--- pushshift_search.py
+++ pushshift_search.py
@@ -2,6 +2,9 @@ import praw
 from psaw import PushshiftAPI
 import datetime as dt
 import json
+import os
+
+downloads_dir = "downloads/"
 
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
@@ -11,18 +14,42 @@ psapi = PushshiftAPI(reddit)
 
 sub_names = ','.join(config['subreddits'])
 
+def parse_comment(comment):
+    return {
+        #"author": comment.author.name,
+        #"author_fullname": comment.author_fullname,
+        #"num_comments": comment.num_comments,
+        "body": comment.body,
+        "created_utc": comment.created_utc,
+        "id": comment.id,
+        "link_id": comment.link_id,
+        "parent_id": comment.parent_id,
+        "score": comment.score,
+        "subreddit_id": comment.subreddit_id,
+        "subreddit": comment.subreddit.display_name
+    }
+
 start_date = int(dt.datetime(2019, 1, 1).timestamp())
 end_date = int(dt.datetime(2020, 9, 23).timestamp())
 
-results_dict = {}
+if not os.path.exists(downloads_dir):
+    os.makedirs(downloads_dir)
+
 for keyword in config['keywords']:
+    print("Starting", keyword)
+
     gen = psapi.search_comments(
         q=keyword, 
-        subreddit='researchchemicals',
+        subreddit=sub_names,
         after=start_date,
         before=end_date
         #aggs="created_utc",
         #frequency="day",
     )
 
-    results_dict[keyword] = list(gen)
+    data = [parse_comment(c) for c in gen]
+
+    print("found", len(data), "results")
+
+    with open(os.path.join(downloads_dir, keyword+'.json'), 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
